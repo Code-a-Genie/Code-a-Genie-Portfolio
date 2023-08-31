fbc5a5848516cb9c794008287314cea848f2e01a
CodeAGenie <nwibeg@gmail.com>
2023-08-31 14:42:43 +0100

    update

---
+++ app.js
@@ -0,0 +1,41 @@
+//activeate hamburger menu
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+
+// activate scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = ()=> {
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+           navLinks.forEach(link => {
+            navLinks.classList.remove('active');
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+           })
+        }
+    })
+    // sticky navBar
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    //remove toggle icon ad navbar when navbar is clicked
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+    
+}
